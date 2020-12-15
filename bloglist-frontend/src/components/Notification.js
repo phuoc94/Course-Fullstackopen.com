52cc82c7e5a1e11a7c2b5c06b40f97aff9130c70
Phuoc Nguyen <phuoc94@hotmail.fi>
2020-12-15 02:47:05 +0200

    5.12: blogilistan frontend, step12

--- bloglist-frontend/src/components/Notification.js
+++ bloglist-frontend/src/components/Notification.js
@@ -1,48 +1,50 @@
 import React, { useState, useImperativeHandle } from 'react'
 
 const Notification = React.forwardRef((props, ref) => {
-    const [emessage, setErrorMessage] = useState(null)
+  const [emessage, setErrorMessage] = useState(null)
 
-    const notiHandler = (message, type) => {
-        setErrorMessage(
-            [message, type]
-        )
-        setTimeout(() => {
-            setErrorMessage(null)
-        }, 5000)
+  const notiHandler = (message, type) => {
+    setErrorMessage(
+      [message, type]
+    )
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+  }
+
+  useImperativeHandle(ref, () => {
+    return {
+      notiHandler
     }
+  })
 
-    useImperativeHandle(ref, () => {
-        return {
-            notiHandler
-        }
-    })
-
-    if (!emessage) {
-        return null
+  if (!emessage) {
+    return null
+  } else {
+    const message = emessage[0]
+    const type = emessage[1]
+    if (type === 'error') {
+      return (
+        <div className="error">
+          {message}
+        </div>
+      )
+    } else if (type === 'success') {
+      return (
+        <div className="success">
+          {message}
+        </div>
+      )
     } else {
-        const message = emessage[0]
-        const type = emessage[1]
-        if (type === 'error') {
-            return (
-                <div className="error">
-                    {message}
-                </div>
-            )
-        } else if (type === 'success') {
-            return (
-                <div className="success">
-                    {message}
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    {message}
-                </div>
-            )
-        }
+      return (
+        <div>
+          {message}
+        </div>
+      )
     }
+  }
 })
+
+Notification.displayName = 'Notification'
 
 export default Notification
--- bloglist-frontend/src/components/Togglable.js
+++ bloglist-frontend/src/components/Togglable.js
@@ -31,6 +31,6 @@ const Togglable = React.forwardRef((props, ref) => {
 Togglable.propTypes = {
   buttonLabel: PropTypes.string.isRequired
 }
-
+Togglable.displayName = 'Togglable'
 
 export default Togglable
