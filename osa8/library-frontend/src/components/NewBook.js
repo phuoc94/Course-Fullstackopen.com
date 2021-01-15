83738e0eadd9d81fde49e45d2b57a1833fe55b37
Phuoc Nguyen <phuoc94@hotmail.fi>
2021-01-15 20:42:16 +0200

    8.22 kirjasuositukset, välimuistin ajantasaisuus

--- osa8/library-frontend/src/components/NewBook.js
+++ osa8/library-frontend/src/components/NewBook.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useMutation } from '@apollo/client'
-import { CREATE_BOOK, ALL_BOOKS, ALL_AUTHORS } from '../queries'
+import { CREATE_BOOK, ALL_BOOKS, ALL_AUTHORS, BOOKS_BY_GEN } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -9,8 +9,18 @@ const NewBook = (props) => {
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
 
+  const getQueries = () => {
+    let refetchQueriesArray = [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }]
+    genres.map(gen => refetchQueriesArray.push(
+      {query: BOOKS_BY_GEN, 
+        variables: { genre: gen } 
+      }
+    ))
+    return refetchQueriesArray
+  } 
+
   const [addBook] = useMutation(CREATE_BOOK, {
-    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }],
+    refetchQueries: getQueries
   })
   if (!props.show) {
     return null
