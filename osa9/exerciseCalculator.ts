a21c74f6141f36e5aa5e2fd1ee52f08a6780a054
Phuoc Nguyen <phuoc94@hotmail.fi>
2021-01-18 21:39:22 +0200

    9.2 Exercise calculator

---
+++ osa9/exerciseCalculator.ts
@@ -0,0 +1,45 @@
+interface Result {
+    periodLength: number;
+    trainingDays: number;
+    success: boolean;
+    rating: number;
+    ratingDescription: string;
+    target: number;
+    average: number;
+}
+type rating = 1 | 2 | 3
+
+const calculateExercises = (list: number[], target: number): Result => {
+    const sum: number = list.reduce(
+        (sum, h) => sum + h,0
+    )
+    const average: number = sum/list.length
+    const ratingDescription: { [key: number]: string } = {
+        1: 'very bad',
+        2: 'not too bad but could be better',
+        3: 'good job',
+    }
+
+    const rating = (): rating => {
+        const ndRating = target * 0.6 // 60% of target
+        if(average >= target){
+            return 3
+        }else if(average >= ndRating){
+            return 2
+        }else{
+            return 1
+        }
+    }
+
+    return { 
+        periodLength: list.length,
+        trainingDays: list.filter(h => h > 0).length,
+        success: rating() === 3,
+        rating: rating(),
+        ratingDescription: ratingDescription[rating()],
+        target: target,
+        average: average
+    }
+}
+
+console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1],2))
