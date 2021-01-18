a39c367638c0ca3395dc525505baa149a5671481
Phuoc Nguyen <phuoc94@hotmail.fi>
2021-01-18 20:02:20 +0200

    9.1 Body mass index

---
+++ osa9/bmiCalculator.ts
@@ -0,0 +1,24 @@
+
+
+const calculateBmi = (height: number, weight: number): string => {
+    const bmi = (weight/(height*height))*10000
+    const Categories: { [index: number]: string; } = {
+        15: 'Very severely underweight',
+        16: 'Severely underweight',
+        18.5: 'Underweight',
+        25: 'Normal (healthy weight)',
+        30: 'Overweight',
+        35: 'Obese Class I (Moderately obese)',
+        40: 'Obese Class II (Severely obese)',
+    }
+
+    const keys = Object.keys(Categories).map(x => parseFloat(x)).sort()
+    for (let y=0; y<keys.length; ++y) {
+        if(bmi <= keys[y]){
+            return Categories[keys[y]]
+        }
+    }
+    return 'Obese Class II (Severely obese)'
+}
+
+console.log(calculateBmi(180, 74))
