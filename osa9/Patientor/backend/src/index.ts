d88d64897d6fe07939a42c534c281f253e2b224d
Phuoc Nguyen <phuoc94@hotmail.fi>
2021-01-20 21:03:21 +0200

    9.10: Patientor backend, step3

--- osa9/Patientor/backend/src/index.ts
+++ osa9/Patientor/backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import diagnosesRouter from './routes/diagnoses';
 import cors from 'cors';
 
 const app = express();
@@ -12,6 +13,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
