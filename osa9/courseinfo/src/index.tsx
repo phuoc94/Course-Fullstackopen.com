ef7a680f92368fb2781890810879203d9e6f85c3
Phuoc Nguyen <phuoc94@hotmail.fi>
2021-01-25 16:44:16 +0200

    9.15

--- osa9/courseinfo/src/index.tsx
+++ osa9/courseinfo/src/index.tsx
@@ -1,16 +1,63 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { PartType } from './types';
+import { CoursePart } from './types';
+
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
 
 const Header: React.FC<{name: string}> = ({name}) => (
     <h1>{name}</h1>
 );
 
-const Part: React.FC<{part: PartType}> = ({part}) => (
-    <p>{part.name} {part.exerciseCount}</p>
-)
+const Part: React.FC<{part: CoursePart}> = ({part}) => {
+  switch (part.name) {
+    case 'Fundamentals':
+      return (
+        <div>
+          <p>name: {part.name}</p>
+          <p>exercise: {part.exerciseCount}</p>
+          <p>description: {part.description}</p>
+          <hr />
+        </div>
+      );
+    case 'Using props to pass data':
+      return (
+        <div>
+          <p>name: {part.name}</p>
+          <p>exercise: {part.exerciseCount}</p>
+          <p>group projects: {part.groupProjectCount}</p>
+          <hr />
+        </div>
+      );
+    case 'Deeper type usage':
+      return (
+        <div>
+          <p>name: {part.name}</p>
+          <p>exercise: {part.exerciseCount}</p>
+          <p>description: {part.description}</p>
+          <p>submissionLink: {part.exerciseSubmissionLink}</p>
+          <hr />
+        </div>
+      );
+    case 'New course':
+      return (
+        <div>
+          <p>name: {part.name}</p>
+          <p>exercise: {part.exerciseCount}</p>
+          <p>description: {part.description}</p>
+          <p>students: {part.students}</p>
+          <hr />
+        </div>
+      );
+    default:
+      return assertNever(part);
+  }
+};
 
-const Content: React.FC<{courseParts: PartType[]}> = ({courseParts}) => (
+const Content: React.FC<{courseParts: CoursePart[]}> = ({courseParts}) => (
   <div>
     {courseParts.map((part) => 
       <Part part={part} key={part.name}/>
@@ -18,7 +65,7 @@ const Content: React.FC<{courseParts: PartType[]}> = ({courseParts}) => (
   </div>
 );
 
-const Total: React.FC<{courseParts: PartType[]}> = ({courseParts}) => (
+const Total: React.FC<{courseParts: CoursePart[]}> = ({courseParts}) => (
   <p>
     Number of exercises{" "}
     {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
@@ -28,19 +75,29 @@ const Total: React.FC<{courseParts: PartType[]}> = ({courseParts}) => (
 
 const App: React.FC = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
-    }
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
+    },
+    {
+      name: "New course",
+      exerciseCount: 14,
+      description: "Confusing description",
+      students: 20
+    },
   ];
 
   return (
--- osa9/courseinfo/src/types.ts
+++ osa9/courseinfo/src/types.ts
@@ -1,4 +1,28 @@
-export interface PartType {
+interface CoursePartBase {
     name: string;
     exerciseCount: number;
-}
+  }
+  
+interface CoursePartOne extends CoursePartBase {
+    name: "Fundamentals";
+    description: string;
+  }
+  
+interface CoursePartTwo extends CoursePartBase {
+    name: "Using props to pass data";
+    groupProjectCount: number;
+  }
+  
+interface CoursePartThree extends CoursePartBase {
+    name: "Deeper type usage";
+    description: string;
+    exerciseSubmissionLink: string;
+  }
+
+  interface CoursePartFour extends CoursePartBase {
+    name: "New course";
+    description: string;
+    students: number;
+  }
+  
+export type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree | CoursePartFour;
